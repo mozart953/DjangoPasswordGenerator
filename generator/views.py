e4a3dbf3fa0ea71a26dcbba913a0ef83150a934c
mozart953 <wilsonnianopop@gmail.com>
2022-05-29 14:28:10 -0600

    subida

---
+++ generator/views.py
@@ -0,0 +1,38 @@
+import re
+from django.shortcuts import render
+import random 
+
+#from django.http import HttpResponse
+
+# Create your views here.
+
+def home (request):
+    return render(request, 'generator/home.html')
+
+def about (request):
+    #return HttpResponse('Hello World')
+    return render(request, 'generator/about.html')
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    
+    if request.GET.get('special'):
+        characters.extend(list('/*-?_'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    #print(valor)
+
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+
+
+    return render(request, 'generator/password.html', {'password': generated_password})
